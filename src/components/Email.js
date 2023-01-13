c63830bea4553b8d992979b6d13666e4a573515f
snehafarkya <snhafarkya@gmail.com>
2023-01-13 18:52:30 +0530

    Occean info

--- src/components/Email.js
+++ src/components/Email.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Email() {
   return (
     <div className="md:w-1/2 mx-4  md:m-auto grid grid-rows-1 place-items-center mb-20">
-      <div class="max-w-3xl p-6  border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <div class="max-w-3xl p-6  bg-white  border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Want to view such amazing websites more?
