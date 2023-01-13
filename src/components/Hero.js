9f21a51cc05d316563c4fe5513f7c635505523d1
snehafarkya <snhafarkya@gmail.com>
2023-01-13 14:49:55 +0530

    An Occean Project

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,8 @@ import occean from '../assets/occean1.jpg'
 
 export default function Hero() {
   return (
-      <div className=" hero-content md:grid md:grid-cols-2 md:mt-56 mt-20 h-screen flex flex-col-reverse ">
-        <div className=' font-bold text-5xl text-left grid grid-col-2 place-items-start ml-20 md:h-32 mt-10' style={{color:'#40a9e9'}}>
+      <div className=" hero-content md:grid md:grid-cols-2 md:mt-56 mt-15 h-screen flex flex-col-reverse ">
+        <div className=' font-bold text-5xl md:text-left text-center grid grid-col-2 md:place-items-start place-items-centermd:ml-20 md:h-32 mt-10' style={{color:'#40a9e9'}}>
           Be the source for<span style={{color:'#ffa04a'}}> Planet's 🌊</span>turning point!
           <button className='mt-20 py-3 px-5 bg-yellow-500 rounded-lg text-gray-100 text-lg'>Deep dive</button>
         </div>
